18f25f5b2346bc49679f4400a3e9b582a38960cf
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2026-03-24 19:10:35 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,6 +93,28 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-17825755545');
+
+            function gtag_report_conversion(url) {
+              var callback = function () {
+                if (typeof(url) != 'undefined') {
+                  window.location = url;
+                }
+              };
+              gtag('event', 'conversion', {
+                'send_to': 'AW-17825755545/BiBvCK_K-Y4cEJnj_bNC',
+                'value': 1.0,
+                'currency': 'RSD',
+                'event_callback': callback
+              });
+              return false;
+            }
+
+            document.addEventListener('click', function(e) {
+              var target = e.target.closest('a[href^="tel:"]');
+              if (target) {
+                gtag_report_conversion();
+              }
+            });
           `}
         </Script>
         <JsonLd data={localBusinessSchema} />
